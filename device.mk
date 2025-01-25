867066ee71cdb6c0b1e94b95a471290801f6c1a2
Cat658011 <cat658011@gmail.com>
2025-01-26 01:38:21 +0500

    m31: Decommonize Fingerprint

--- device.mk
+++ device.mk
@@ -39,3 +39,10 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml \
     $(DEVICE_PATH)/configs/audio/mixer_paths_rc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_rc.xml \
     $(DEVICE_PATH)/configs/audio/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml
+
+# Fingerprint
+PRODUCT_PACKAGES += \
+     android.hardware.biometrics.fingerprint-service.samsung
+
+PRODUCT_COPY_FILES += \
+     $(DEVICE_PATH)/configs/keylayout/uinput-sec-fp.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-sec-fp.kl
