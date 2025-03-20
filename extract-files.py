398aacda79993b732c2338540463d1eb68409f0d
Tim Zammermann <tim@linux4.de>
2025-03-20 15:30:28 +0500

    m31: Switch to py extract-files

---
+++ extract-files.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2025 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'device/samsung/universal9611-common',
+    'hardware/samsung_slsi-linaro/exynos',
+    'hardware/samsung_slsi-linaro/graphics',
+    'vendor/samsung/universal9611-common',
+]
+
+blob_fixups: blob_fixups_user_type = {
+    (
+        'vendor/lib/sensors.grip.so',
+        'vendor/lib/sensors.inputvirtual.so',
+        'vendor/lib/sensors.sensorhub.so',
+        'vendor/lib64/sensors.grip.so',
+        'vendor/lib64/sensors.inputvirtual.so',
+        'vendor/lib64/sensors.sensorhub.so',
+    ): blob_fixup()
+        .remove_needed('libhidltransport.so')
+        .add_needed('libutils-v32.so')
+        .binary_regex_replace(b'_ZN7android6Thread3runEPKcim', b'_ZN7utils326Thread3runEPKcim'),
+    (
+        'vendor/lib/libsensorlistener.so',
+        'vendor/lib64/libsensorlistener.so',
+    ): blob_fixup()
+        .add_needed('libshim_sensorndkbridge.so'),
+} # fmt: skip
+
+module = ExtractUtilsModule(
+    'm31',
+    'samsung',
+    namespace_imports=namespace_imports,
+    blob_fixups=blob_fixups,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device_with_common(
+        module, 'universal9611-common', module.vendor
+    )
+    utils.run()
